b7ae4d99cf03dd421a848b5089341f31ad218e4d
Nobert-Muma <mumanobert17@gmail.com>
2024-08-31 22:34:28 +0300

    Attributes and properties in Python

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 APPROVED_BREEDS = [
     "Mastiff",
@@ -12,4 +11,31 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        self.name=name
+        self.breed=breed
+    def get_name(self):
+        print("Retrieving the name of the dog.")
+        return self._name
+    def set_name(self, name):
+        if name is None:
+            self._name=name
+        elif (type(name) == str) and (1 <= len(name) <= 25):
+            print(f"Setting the name of the dog to {name}")
+            self._name=name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    def get_breed(self):
+        print("Retrieving the breed of the dog.")
+        return self._breed
+    def set_breed(self, breed):
+        if breed is None:
+            self._breed=None
+        elif breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            print(f"Setting the breed of the dog to {breed}")
+            self._breed=breed
+    name=property(get_name, set_name)
+    breed=property(get_breed, set_breed)
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,29 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        self.name=name
+        self.job=job
+    def get_name(self):
+        print("Retrieving the name of the person.")
+        return self._name
+    def set_name(self, name):
+        if name is None:
+            self._name=None
+        elif (type(name)==str) and 1 <= len(name) <=25:
+            print(f"Setting name to {name}")
+            self._name=name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    def get_job(self):
+        print("Retrieving the name of the job.")
+        return self._job
+    def set_job(self, job):
+        if job is None:
+            self._job=None
+        elif job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job=job
+    name=property(get_name, set_name)
+    job=property(get_job, set_job)
